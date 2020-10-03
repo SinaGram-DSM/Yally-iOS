0c8469e271d96ab1aac19786c70436e867fa7ded
Ga-yo <39812@naver.com>
2020-10-03 23:07:58 +0900

    Feat : Set Networking logic

---
+++ Yally/Yally/Source/Model/Token.swift
@@ -0,0 +1,13 @@
+//
+//  Token.swift
+//  Yally
+//
+//  Created by 이가영 on 2020/10/03.
+//
+
+import Foundation
+
+struct Token: Codable {
+    var accesstoekn: String
+    var refreshToken: String
+}
---
+++ Yally/Yally/Source/Networking/AuthAPI.swift
@@ -0,0 +1,8 @@
+//
+//  AuthAPI.swift
+//  Yally
+//
+//  Created by 이가영 on 2020/10/03.
+//
+
+import Foundation
