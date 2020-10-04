73648e3e662a15bae6c34d99da45bde32bdda4ef
Ga-yo <39812@naver.com>
2020-10-04 22:58:56 +0900

    Feat : Networking

--- Yally/Yally/Source/Networking/YallyURL.swift
+++ Yally/Yally/Source/Networking/YallyURL.swift
@@ -9,13 +9,21 @@ import Foundation
 import Security
 
 enum YallyURL {
-    case signIn(_ eamil: String, _ password: String)
-    case authCode(_ email: String)
-    case authConfirm(_ code: String)
-    case signUp(_ email: String, _ password: String, _ nickname: String, _ age: Int)
-    case resetCodeToEmail(_ email: String)
-    case modifyPassword(_ email: String, _ code: String, _ password: String)
-    case refreshToken
+        case signIn
+        case authCode
+        case authConfirm
+        case signUp
+        case resetCodeToEmail
+        case modifyPassword
+        case refreshToken
+
+//    case signIn(_ eamil: String, _ password: String)
+//    case authCode(_ email: String)
+//    case authConfirm(_ code: String)
+//    case signUp(_ email: String, _ password: String, _ nickname: String, _ age: Int)
+//    case resetCodeToEmail(_ email: String)
+//    case modifyPassword(_ email: String, _ code: String, _ password: String)
+//    case refreshToken
 
     func path() -> String {
         switch self {
@@ -33,6 +41,19 @@ enum YallyURL {
             return "/user/auth/password"
         case .refreshToken:
             return "/user/auth/refresh"
+        }
+    }
+
+    func headers() -> [String: String]? {
+        switch self {
+        case .signUp, .authCode, .authConfirm, .resetCodeToEmail, .modifyPassword:
+            return nil
+        case .refreshToken:
+            guard let token = currentToken?.refreshToken else { return nil }
+            return ["Authorization": token]
+        default:
+            guard let token = currentToken?.accesstoekn else { return nil }
+            return ["Authorization": token]
         }
     }
 }
