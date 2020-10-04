852cadeb769c221cb1d9444f57adae063d306790
Ga-yo <39812@naver.com>
2020-10-04 22:58:19 +0900

    Feat : Utils

--- Yally/Yally/Source/Util/Extension.swift
+++ Yally/Yally/Source/Util/Extension.swift
@@ -20,3 +20,46 @@ extension UIColor {
             )
         }
 }
+
+extension UIViewController {
+    func nextScene(identifier: String) {
+        let viewController = self.storyboard?.instantiateViewController(withIdentifier: identifier)
+        navigationController?.pushViewController(viewController!, animated: true)
+    }
+
+    func setButton(_ button: UIButton) {
+        var gradientLayer: CAGradientLayer!
+
+        gradientLayer = CAGradientLayer()
+        gradientLayer.frame = button.bounds
+        gradientLayer.colors = [UIColor().hexUIColor(hex: "4776e6").cgColor, UIColor().hexUIColor(hex: "8e54e9").cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        gradientLayer.cornerRadius = 20
+
+        button.layer.insertSublayer(gradientLayer, at: 0)
+        button.layer.cornerRadius = 20
+        button.tintColor = .white
+    }
+
+    func defuatlBtn(_ button: UIButton) {
+        button.backgroundColor = .gray
+        button.tintColor = .white
+        button.layer.cornerRadius = 20
+        button.isEnabled = false
+    }
+}
+
+struct YallyFilter {
+    static func checkEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: email)
+    }
+
+    static func checkPw(_ pw: String) -> Bool {
+        let pwRegEx = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z).{8}$"
+        let pwTest = NSPredicate(format: "SELF MATCHES %@", pwRegEx)
+        return pwTest.evaluate(with: pw)
+    }
+}
