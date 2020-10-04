018fb0b1f0c67d02e6f80c2b6a123378945d7c95
Ga-yo <39812@naver.com>
2020-10-04 22:58:56 +0900

    Feat : Networking

--- Yally/Yally/Source/Networking/HTTPClient.swift
+++ Yally/Yally/Source/Networking/HTTPClient.swift
@@ -9,22 +9,23 @@ import Foundation
 import Alamofire
 
 class HTTPClient {
+
     let baseURI = "http://13.125.238.84:81"
 
-    func get(path: String, param: [String: Any], headers: HTTPHeaders) -> DataRequest {
-        return AF.request(baseURI + path, method: .get, parameters: param, encoding: JSONEncoding.prettyPrinted, headers: headers, interceptor: nil)
+    func get(path: String, param: [String: Any], headers: [String:String]) -> DataRequest {
+        return AF.request(baseURI + path, method: .get, parameters: param, encoding: JSONEncoding.prettyPrinted, headers: HTTPHeaders(headers), interceptor: nil)
     }
 
-    func put(path: String, param: [String: Any], headers: HTTPHeaders) -> DataRequest {
-        return AF.request(baseURI + path, method: .put, parameters: param, encoding: JSONEncoding.prettyPrinted, headers: headers, interceptor: nil)
+    func put(path: String, param: [String: Any], headers: [String:String]) -> DataRequest {
+        return AF.request(baseURI + path, method: .put, parameters: param, encoding: JSONEncoding.prettyPrinted, headers: HTTPHeaders(headers), interceptor: nil)
     }
 
-    func delete(path: String, param: [String: Any], headers: HTTPHeaders) -> DataRequest {
-        return AF.request(baseURI + path, method: .delete, parameters: param, encoding: JSONEncoding.prettyPrinted, headers: headers, interceptor: nil)
+    func delete(path: String, param: [String: Any], headers: [String:String]) -> DataRequest {
+        return AF.request(baseURI + path, method: .delete, parameters: param, encoding: JSONEncoding.prettyPrinted, headers: HTTPHeaders(headers), interceptor: nil)
     }
 
-    func post(path: String, param: [String: Any], headers: HTTPHeaders) -> DataRequest {
-        return AF.request(baseURI + path, method: .post, parameters: param, encoding: JSONEncoding.prettyPrinted, headers: headers, interceptor: nil)
+    func post(path: String, param: [String: Any], headers: [String:String]) -> DataRequest {
+        return AF.request(baseURI + path, method: .post, parameters: param, encoding: JSONEncoding.prettyPrinted, headers: HTTPHeaders(headers), interceptor: nil)
     }
 }
 
