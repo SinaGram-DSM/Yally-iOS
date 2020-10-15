4f87894f065ca4cc6bfcf5ea4f55afa74a249ff3
JI-doo <awt0311@gmail.com>
2020-10-15 13:59:26 +0900

    Feat: ProfileNetworking

--- Yally/Yally/Source/Networking/YallyURL.swift
+++ Yally/Yally/Source/Networking/YallyURL.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 enum YallyURL {
+    case listeningValue
+    case listenerValue
+
+    func path() -> String {
+        let baseURL = "http://13.125.238.84:81"
+        switch self {
+        case .listeningValue:
+            return baseURL + "/profile/<email>"
+        case .listenerValue:
+            return baseURL + "/profile/<email>"
+        }
+    }
 
 }
