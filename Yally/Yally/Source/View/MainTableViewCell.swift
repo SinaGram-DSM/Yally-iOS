36eaa9d437cf33f9a23c2c7f72eb1e8cf2f8156c
Ga-yo <39812@naver.com>
2020-10-18 22:24:06 +0900

    Feat : maincell clean

--- Yally/Yally/Source/View/MainTableViewCell.swift
+++ Yally/Yally/Source/View/MainTableViewCell.swift
@@ -20,62 +20,62 @@ class MainTableViewCell: UITableViewCell {
     @IBOutlet weak var doComment: UIButton!
     @IBOutlet weak var sliderBar: UISlider!
     @IBOutlet weak var timeLabel: UILabel!
-    
+
     private var onGesture: Bool = false
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+
         sliderBar.isHidden = true
         timeLabel.isHidden = true
-        
-        setupTextView()
-        
+
+        setUpUI()
+
         let tapGestureOn = UITapGestureRecognizer(target: self, action: #selector(touchToOn))
         backImageView?.addGestureRecognizer(tapGestureOn)
         backImageView?.isUserInteractionEnabled = true
     }
-    
-    func setupTextView() {
+
+    func setUpUI() {
         backImageView.backgroundColor = .black
         backImageView.alpha = 0.7
         backImageView.image = UIImage(named: "나은.jpeg")//
-        
+
         mainTextView.textAlignment = .center
         mainTextView.isEditable = false
         mainTextView.isSelectable = false
         mainTextView.textColor = .black
         mainTextView.text = "슬라이더 등장이요슬라이더 등장이요슬라이더 등장이요슬라이더 등장이요슬"//
-        
+
         let fixedWidth = mainTextView.frame.size.width
         let newSize = mainTextView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
         mainTextView.frame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
     }
-    
+
     @objc func touchToOn() {
         if !onGesture {
             //음원 재생
-            
+
             backImageView.alpha = 0.3
-            
+
             sliderBar.isHidden = false
             timeLabel.isHidden = false
 
             onGesture = true
-            
+
         } else {
             //음원 재생
-            
+
             backImageView.alpha = 0.7
-            
+
             sliderBar.isHidden = true
             timeLabel.isHidden = true
-           
+
             onGesture = false
         }
     }
-    
-    override var isSelected: Bool{
+
+    override var isSelected: Bool {
         didSet {
             doYally.tintColor = isSelected ? UIColor.purple
                 : UIColor.gray
@@ -83,11 +83,11 @@ class MainTableViewCell: UITableViewCell {
                 : UIColor.gray
         }
     }
-    
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-    
+
 }
