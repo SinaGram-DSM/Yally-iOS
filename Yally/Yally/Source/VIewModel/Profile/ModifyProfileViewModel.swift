237b03f72e039ac92473279aa43c4f9090a051a3
JI-doo <awt0311@gmail.com>
2021-01-07 16:41:23 +0900

    Fix: ModifyProfileViewModel

--- Yally/Yally/Source/VIewModel/Profile/ModifyProfileViewModel.swift
+++ Yally/Yally/Source/VIewModel/Profile/ModifyProfileViewModel.swift
@@ -11,40 +11,40 @@ import RxSwift
 import RxCocoa
 
 class ModifyProfileViewModel: ViewModelType {
-    
+
     let disposeBag = DisposeBag()
-    
+
     struct input {
         let nickName: Driver<String>
-        let userImage: Driver<String>
+        let userImage: Driver<Data>
         let doneTap: Signal<Void>
     }
-    
+
     struct output {
         let result: Signal<String>
         let isEnabled: Driver<Bool>
     }
-    
+
     func transform(_ input: input) -> output {
         let api = ProfileAPI()
         let result = PublishSubject<String>()
         let info = Driver.combineLatest(input.nickName, input.userImage)
         let isEnabled = info.map { !$0.0.isEmpty }
         
-        input.doneTap.withLatestFrom(info).asObservable().subscribe(onNext: { nickName, image in
-            api.putModifyProfile().subscribe(onNext: { (response, statusCode) in
-                switch statusCode {
-                case .success:
+        input.doneTap.withLatestFrom(info).asObservable().subscribe(onNext: { nickname, image in
+            api.formData(.modifyprofile, param: ["nickname" : nickname], img: image ?? nil).responseJSON { (response) in
+                switch response.response?.statusCode {
+                case 200:
                     result.onCompleted()
-                case .noHere:
+                case 404:
                     result.onNext("변경실패")
                 default:
                     result.onNext("default")
                 }
-            }).disposed(by: self.disposeBag)
+            }
         }).disposed(by: disposeBag)
-        
+
         return output(result: result.asSignal(onErrorJustReturn: ""), isEnabled: isEnabled.asDriver())
-        
+
     }
 }
